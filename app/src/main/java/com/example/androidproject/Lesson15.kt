1a6193a80073fb4a88f5890ff240c0cf8a5f7995
Kolyan99 <shuknikolay99@gmail.com>
2022-11-03 19:59:52 +0300

    Lesson15

---
+++ app/src/main/java/com/example/androidproject/Lesson15.kt
@@ -0,0 +1,11 @@
+package com.example.androidproject
+
+ open class Lesson15 {
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            print("hello world")
+        }
+    }
+}
