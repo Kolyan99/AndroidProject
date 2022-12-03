1e97c77c435f839020ff0a2308dd40712f24b963
Kolyan99 <shuknikolay99@gmail.com>
2022-12-03 21:07:12 +0300

    lesson20

--- app/src/main/java/com/example/androidproject/ItemsFragment.kt
+++ app/src/main/java/com/example/androidproject/ItemsFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidproject.adapter.ItemsAdapter
@@ -16,6 +18,8 @@ import com.example.androidproject.listener.ItemListener
 class ItemsFragment : Fragment(), ItemListener {
 
     private lateinit var itemsAdapter: ItemsAdapter
+
+    private val viewModel: ItemsViewModel by viewModels()
 
   //  private val itemsAdapter2: ItemsAdapter = ItemsAdapter()// так делать можно, но не нужно)
 
@@ -36,61 +40,37 @@ class ItemsFragment : Fragment(), ItemListener {
         recucleView.layoutManager = LinearLayoutManager(context)
         recucleView.adapter = itemsAdapter
 
-        val listItems = listOf<ItemsModel>(
-            ItemsModel(R.drawable.fruit,
-            "Android",
-            "26.02.2023"),
-            ItemsModel(
-                R.drawable.fruit,
-                "C++",
-                "26.02.2023"),
-            ItemsModel(
-                R.drawable.img,
-                "C",
-                "26.02.2024"),
-            ItemsModel(
-                R.drawable.img_1,
-                "Java",
-                "26.02.2026"),
-            ItemsModel(
-                R.drawable.fruit,
-                "IOS",
-                "26.04.2020"),
-            ItemsModel(
-                R.drawable.fruit,
-                "PHP",
-                "26.02.2023"),
-            ItemsModel(
-                R.drawable.fruit,
-                "JS",
-                "22.02.2021")
-        )
-        itemsAdapter.submitList(listItems.toList())
+        viewModel.getData()
 
+        viewModel.items.observe(viewLifecycleOwner){ listItems ->
+            itemsAdapter.submitList(listItems)
+        }
+
+        viewModel.msg.observe(viewLifecycleOwner){msg ->
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.bundel.observe(viewLifecycleOwner){navBundle ->
+            val detailsFragment = DetailsFragment()
+            val bundle = Bundle()
+            bundle.putString(getString(R.string.name), navBundle.name)
+            bundle.putString(getString(R.string.date), navBundle.data)
+            bundle.putInt(getString(R.string.imageView), navBundle.image)
+            detailsFragment.arguments = bundle
+
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.activity_container, DetailsFragment())
+                .addToBackStack(getString(R.string.OnBoarding))
+                .commit()
+        }
     }
 
     override fun onClick() {
-       Toast.makeText(context, " ImageView clicked", Toast.LENGTH_SHORT).show()
+       viewModel.imageViewClicked()
     }
 
     override fun onElementSelected(name: String, date: String, imageView: Int) {
-        val detailsFragment = DetailsFragment()
-        val bundle = Bundle()
-        bundle.putString("name", name)
-        bundle.putString("date", date)
-        bundle.putInt("imageView", imageView)
-        detailsFragment.arguments = bundle
-
-         // TODO add метод мы больше не используем
-        // теперь мы всегда будем использовать replace
-        // replace всегда будет иметь или addToBackstack, чтобы
-        // мочь вернуться назад или же не будет, чтобы
-        // мы вернулись назад.
-        parentFragmentManager
-            .beginTransaction()
-            .replace(R.id.activity_container, DetailsFragment())
-            .addToBackStack("Onboarding")
-            .commit()
+     viewModel.elementClicked(name,date,imageView)
     }
-
 }
---
+++ app/src/main/java/com/example/androidproject/ItemsViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.androidproject
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.androidproject.adapter.model.ItemsModel
+
+class ItemsViewModel: ViewModel() {
+
+    private val _items = MutableLiveData<List<ItemsModel>>()
+    val items: LiveData<List<ItemsModel>> = _items
+
+    private val _msg = MutableLiveData<String>()
+    val msg: LiveData<String> = _msg
+
+    private val _bundle = MutableLiveData<NavigateWithBundel>()
+    val bundel: LiveData<NavigateWithBundel> = _bundle
+
+    fun getData(){
+        val listItems = listOf<ItemsModel>(
+            ItemsModel(R.drawable.fruit,
+                "Android",
+                "26.02.2023"),
+            ItemsModel(
+                R.drawable.fruit,
+                "C++",
+                "26.02.2023"),
+            ItemsModel(
+                R.drawable.img,
+                "C",
+                "26.02.2024"),
+            ItemsModel(
+                R.drawable.img_1,
+                "Java",
+                "26.02.2026"),
+            ItemsModel(
+                R.drawable.fruit,
+                "IOS",
+                "26.04.2020"),
+            ItemsModel(
+                R.drawable.fruit,
+                "PHP",
+                "26.02.2023"),
+            ItemsModel(
+                R.drawable.fruit,
+                "JS",
+                "22.02.2021")
+        )
+        _items.value = listItems
+    }
+
+    fun imageViewClicked(){
+        _msg.value = " ImageView clicked"
+
+    }
+
+    fun elementClicked(name: String, date: String, imageView: Int){
+        _bundle.value = NavigateWithBundel(
+            name = name,
+            data= date,
+            image = imageView
+        )
+    }
+}
+
+data class NavigateWithBundel(
+    val image: Int,
+    val name: String,
+    val data: String
+)
